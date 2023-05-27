a4a3537e434b714303f5494dd06e55c87252cb58
itsdivanshu <125489189+itsdivanshu@users.noreply.github.com>
2023-05-27 15:09:47 +0530

    Create index.php

---
+++ res/index.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$course = $_POST['course'];
+$roll = $_POST['roll'];
+$gender = $_POST['gen'];
+
+echo "Name : $name<br>Email : $email<br>Course : $course<br>Roll No : $roll<br>Gender : $gender";
+
+
+?>
